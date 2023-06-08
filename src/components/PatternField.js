fece04dad19987028b477c3efc307f535ab20319
Mik-Halo <sprone3@gmail.com>
2023-06-08 20:06:45 +0300

    disabled eslint

--- src/components/PatternField.js
+++ src/components/PatternField.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState, useRef } from 'react';
 import PatternPad from './PatternPad';
 import { BsFillPlayFill, BsFillStopFill } from 'react-icons/bs';
